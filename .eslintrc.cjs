653ca707e01590a7d36efbd12d04c96aaa82547d
elibrody <85048617+elibrody@users.noreply.github.com>
2024-05-14 21:30:37 -0500

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@
  */
 module.exports = {
   root: true,
-#  extends: ['@antfu', './.eslintrc-auto-import.json', '@unocss'],
+//  extends: ['@antfu', './.eslintrc-auto-import.json', '@unocss'],
   extends: ['./.eslintrc-auto-import.json', '@unocss'],
 
   rules: {
